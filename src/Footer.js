95323d015efd78b973034b163e13f178f5dfabd1
rubaalbrezat <rubaalbrezat@gmail.com>
2022-07-24 23:45:30 +0300

    aaa

---
+++ src/Footer.js
@@ -0,0 +1,8 @@
+import React  from "react"; 
+class Footer extends React.Component{
+    render(){
+        return(<p>Done by Ruba</p>)
+    }
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+ class Header extends React.Component{
+    render(){
+        return(
+            <h1>unicorn-zoo</h1>
+        )
+    }
+ }
+ export default Header;
