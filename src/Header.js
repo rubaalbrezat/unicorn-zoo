7a4f43fe40584e90be5f4eb78533fe31db68c9ff
rubaalbrezat <rubaalbrezat@gmail.com>
2022-07-29 02:13:20 +0300

    lab02

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
+import NavBar from './NavBar';
 
 
  class Header extends React.Component{
     render(){
         return(
-            
+            <>
+            <NavBar/>
 
             <h1>unicorn-zoo</h1>
+            </>
         )
     }
  }
