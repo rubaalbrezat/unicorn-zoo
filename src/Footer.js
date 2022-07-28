d23cb6ad6a7fa2d7fda34dac992559b6f9e4f6bb
rubaalbrezat <rubaalbrezat@gmail.com>
2022-07-28 23:37:54 +0300

    done task 1

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,8 @@
 import React  from "react"; 
 class Footer extends React.Component{
     render(){
-        <>return(<p>Done by Ruba</p>)
-        </>
+        return( <><p>Done by Ruba</p></>)
+       
         
     }
 }
